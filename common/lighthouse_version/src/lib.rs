e671e9dbf55f132e3189f618d2779a133f583086
Shane Bammel <sjb933@gmail.com>
2026-03-28 01:49:43 -0500

    Prepare v2.5.1 patch release

--- common/lighthouse_version/src/lib.rs
+++ common/lighthouse_version/src/lib.rs
@@ -17,8 +17,8 @@ pub const VERSION: &str = git_version!(
         // NOTE: using --match instead of --exclude for compatibility with old Git
         "--match=thiswillnevermatchlol"
     ],
-    prefix = "Lighthouse-Pulse/v2.5.0-",
-    fallback = "Lighthouse-Pulse/v2.5.0"
+    prefix = "Lighthouse-Pulse/v2.5.1-",
+    fallback = "Lighthouse-Pulse/v2.5.1"
 );
 
 /// Returns `VERSION`, but with platform information appended to the end.
